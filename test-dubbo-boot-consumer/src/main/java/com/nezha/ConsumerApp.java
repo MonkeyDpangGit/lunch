7e1a762f8176bae2b32e813f26585b9c3dd2158f
MonkeyDpangGit <42015973+MonkeyDpangGit@users.noreply.github.com>
2020-02-24 11:37:03 +0800

    dubbo-demo

--- test-dubbo-boot-consumer/src/main/java/com/nezha/ConsumerApp.java
+++ test-dubbo-boot-consumer/src/main/java/com/nezha/ConsumerApp.java
@@ -5,12 +5,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Hello world!
- *
  */
 @SpringBootApplication
 public class ConsumerApp {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         new SpringApplicationBuilder(ConsumerApp.class).run(args);
     }
 }
